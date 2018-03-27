09bac8a97e859ccc2505db649af13f0eb7c0b886
AaronStonewins <appu.253@gmail.com>
2018-03-27 23:18:04 +0530

    Add files via upload

--- Assignment1/Problem3PRNN.py
+++ Assignment1/Problem3PRNN.py
@@ -10,90 +10,91 @@ import re
 import math
 import numpy as np
 s=".\datasets_assignement1\P3_training_data.csv"
-bagOfWords=[]
+#bagOfWords=[]
+
+
+def getFreq(word,d,count):
+    if word in d:
+        return math.log(d[word]/count)
+    else:
+        return math.log(1/count)
+    
+    
+class1={};class2={}
 label=[]
+countPos=0;countNeg=0
 with open(s,'r') as csvfile:
      readCSV=csv.reader(csvfile,delimiter=',')
      for row in readCSV:
-         b=re.findall(r'\b\w+\b',row[0])
-         label.append(b[0])
-         bagOfWords.append(b[1:len(b)])
-
-
-numOfPos=0
-n=len(label)
-for i in range(n):
-    if label[i]=='Pos':
-        numOfPos=numOfPos+1
-        
-Class1=[];Class2=[]      
-for i in range(numOfPos):
-    m=len(bagOfWords[i])
-    for j in range(m):
-        Class1.append(bagOfWords[i][j])
-        
-        
-for i in range(numOfPos,n,1):
-    m=len(bagOfWords[i])
-    for j in range(m):
-        Class2.append(bagOfWords[i][j])
-
-
-
-m1=len(Class1)
-d1={}
-for i in range(m1):
-    d1[Class1[i]]=0
-V1=len(d1)
-C1=V1+m1
-for i in range(m1):
-    d1[Class1[i]]=(1/C1)
-for i in range(m1):
-    d1[Class1[i]]=d1[Class1[i]]+(1/C1)
-
-d2={};m2=len(Class2)
-for i in range(m2):
-    d2[Class2[i]]=0
-V2=len(d2)
-C2=V2+m2
-for i in range(m2):
-    d2[Class2[i]]=(1/C2)
-for i in range(m2):
-    d2[Class2[i]]=d2[Class2[i]]+(1/C2)
-    
-print(d1) 
-
-
-s1=input("Enter the file's destination:")
-
-bagOfWordsTest=[]
-
-
+         b=re.findall(r'\w+',row[0])
+         label=b.pop(0)
+         if label=='Pos':
+             for word in b:
+                 if word not in class1:
+                     class1[word]=2
+                 else:
+                     class1[word]=class1[word]+1
+             countPos=countPos+len(b)
+         else:
+             for word in b:
+                 if word not in class2:
+                     class2[word]=2
+                 else:
+                     class2[word]=class2[word]+1
+             countNeg=countNeg+len(b)
+             
+             
+lenOfClass1=len(class1)
+lenOfClass2=len(class2)
+countTotal=countPos+countNeg  
+lenTotal=lenOfClass1+lenOfClass2   
+s1=".\P3_test_data.csv"
+L=[];countOne=0;countZero=0
 with open(s1,'r') as csvfile:
      readCSV=csv.reader(csvfile,delimiter=',')
      for row in readCSV:
-         b=re.findall(r'\b\w+\b',row[0])
-         bagOfWordsTest.append(b[1:len(b)])
+         P1=0;P2=0
+         b=re.findall(r'\w+',row[0])
+         for word in b:
+             P1=P1+getFreq(word,class1,countPos+lenOfClass1)
+             P2=P2+getFreq(word,class2,countNeg+lenOfClass2)
+         if P1>P2:
+             L.append(1)
+             countOne=countOne+1
+         else:
+             L.append(0)
+             countZero=countZero+1
 
 
-N=len(bagOfWordsTest)
-P1=1;P2=1
-for j in range(N):
-    if bagOfWordsTest[j] in d1 and bagOfWordsTest[j] in d2:
-        P1=P1*d1[bagOfWordsTest[j]]
-        P2=P2*d2[bagOfWordsTest[j]]
-    else:
-        P1=P1/C1
-        P2=P2/C2
+print(countOne)
+print(countZero)
+             
 
 
-Z=math.log(P1/P2)
-if np.sign(Z):
-    print("Positive")
-else:
-    print("Negative")
 
 
+
+
+
+
+
+#with open(s1,'r') as csvfile:
+#     readCSV=csv.reader(csvfile,delimiter=',')
+#     for row in readCSV:
+#         b=re.findall(r'\b\w+\b',row[0])
+#         bagOfWordsTest.append(b[1:len(b)])
+#
+#
+#N=len(bagOfWordsTest)
+#P1=1;P2=1
+#for j in range(N):
+#    if bagOfWordsTest[j] in d1 and bagOfWordsTest[j] in d2:
+#        P1=P1*d1[bagOfWordsTest[j]]
+#        P2=P2*d2[bagOfWordsTest[j]]
+#    else:
+#        P1=P1/C1
+#        P2=P2/C2
+#
 
 
 
